2ff00302160ba7069e601126f61f4cccff0f2381
RizalPradana02 <kamakaco404@gmail.com>
2022-03-21 08:25:52 +0700

    tugas ke 2

---
+++ tugasCodingan.py
@@ -0,0 +1,26 @@
+#list
+nilai = [2,3,"b",False]
+for i in(nilai):
+    print(i)
+nilai[0]=5
+print(nilai)
+nilai.pop(0)
+print(nilai)
+nilai.append(7)
+print(nilai)
+#tuple
+nilai1 = (4,5,"rizal",True)
+for z in(nilai1) :
+    print(z)
+#set
+nilai2 = {2,7,"ical",False}
+for j in(nilai2):
+    print(j)
+nilai2.add(5)
+print(nilai2)
+#dictionery
+nilai3 = {"nama":"rizal","umur":20}
+for w in(nilai3) :
+    print(w)
+nilai3["mahasiswa"]="unsulbar"
+print(nilai3)
